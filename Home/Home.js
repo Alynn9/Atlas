c7ffd2dc29ebcb82a763cee63300f21880130fba
Alynn Torossian <torossianalynn7@outloook.com>
2023-04-27 21:10:59 +0300

    hoem uodated by css isnt working

--- Home/Home.js
+++ Home/Home.js
@@ -1,5 +1,14 @@
-$("input").on("change", function () {
-  $(".container").toggleClass("blue");
-});
-
-
+const text = document.querySelector(".sec-text");
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = " Drama";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = " 3D";
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "Horror";
+  }, 8000);
+};
+textLoad();
+setInterval(textLoad, 12000);
